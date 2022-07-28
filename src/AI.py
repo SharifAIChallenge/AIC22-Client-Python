eddd80ce93e448a26b6abeb1467055ac6d7ba1c9
ali <ali.sdfi.dev@gmail.com>
2022-07-28 17:16:20 +0430

    update: use logging methods

--- src/AI.py
+++ src/AI.py
@@ -1,9 +1,18 @@
 from src.client import GameClient
 from src.model import GameView
+from src import tools
+
+log = tools.Logger()
+
+
+
+
 
 
 def get_thief_starting_node(view: GameView) -> int:
     # write your code here
+    log.setup(view.viewer)
+
     return 2
 
 
@@ -21,6 +30,7 @@ class AI:
 
     def thief_move_ai(self, view: GameView) -> int:
         # write your code here
+        log.setup(view.viewer)
         message = ''
         for m in range(len(view.visible_agents)):
             message = message  + '0'
@@ -29,5 +39,6 @@ class AI:
 
     def police_move_ai(self, view: GameView) -> int:
         # write your code here
+        log.setup(view.viewer)
         self.phone.send_message('00101001')
         return 1
